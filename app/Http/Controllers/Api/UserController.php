44f39b38a5f7979b39017ef0e1a0a587f72d0758
atpfreelancer <atpfreelancer@gmail.com>
2023-06-08 15:31:22 +0300

    up

--- app/Http/Controllers/Api/UserController.php
+++ app/Http/Controllers/Api/UserController.php
@@ -46,7 +46,7 @@ class UserController extends Controller
 
         $user = User::create($validatedData);
 
-        return response()->json(['message' => 'User created successfully', 'data' => $user], 201);
+        return response()->json(['message' => 'User created successfully', 'data' => $user], 200);
     }
 
     /**
